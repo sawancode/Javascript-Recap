eb5e7ccbd5a995577e8084132fc3dafdd63f3281
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2025-07-06 07:20:47 +0000

    Conversion of DataTypes

---
+++ DataTypeConversion/ConversionOperation.js
@@ -0,0 +1,26 @@
+{/* Type Conversion*/}
+
+let scoreType = "33abc";
+let nullType = null;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// Type Conversion
+// let typeConversion = Number(nullType)
+// console.log(typeConversion)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+
+let isLoggedIn = "Javascript";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false.
+// "" => false.
+// "Javascript" => true.
+
+let stringVariable = 3;
+let stringConversion = String(stringVariable)
+console.log(typeof stringConversion)
+
